00657f48cdbb1a58286d1c853e025da3db401b0b
이예진 <iyejin@iyejin-ui-MacBookAir.local>
2022-10-20 20:13:15 +0900

    test

---
+++ test.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        
+        <select>
+            <option>
+            ggg
+        </option>
+        </select>
+        <input type="text">
+        <?php
+        echo 'test';
+        ?>
+        ㄹㄷㄹㄷ
+        <script>
+
+        </script>
+    </body>
+</html>
